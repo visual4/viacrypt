c64ad264c0053e4eecb93245806cc8e3acd6ceee
brafreider <bjoern_rafreider@visual4.de>
2018-12-11 17:15:18 +0100

    added German po file first version

--- xgettext.sh
+++ xgettext.sh
@@ -13,7 +13,7 @@ abspath=`pwd | sed 's/\//\\\\\\//g'`
 sed -i.tmp "s/$abspath\///" locale/messages.pot
 rm locale/messages.pot.tmp
 
-for lang in en br; do
+for lang in en br de; do
 	msgmerge -q locale/$lang/messages.po locale/messages.pot > locale/$lang/messages.po.new || fail msgmerge
 	mv locale/$lang/messages.po.new locale/$lang/messages.po
 	po2json --pretty --domain=messages locale/$lang/messages.po locale/$lang/messages.json || fail po2json
